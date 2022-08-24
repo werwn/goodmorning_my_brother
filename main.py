0cbabc35587d43293976a015c2093dce2ce88693
werwn <104704164+werwn@users.noreply.github.com>
2022-08-24 16:35:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
-user_id1 = os.environ["USER_ID"]
+user_id1 = os.environ["USER_ID1"]
 template_id = os.environ["TEMPLATE_ID"]
 
 
